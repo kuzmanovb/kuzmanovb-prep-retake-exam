61cb1d7d9db37a68376c9fa8fd404c3c676e9aac
Boyan Kuzmanov <50366388+kuzmanovb@users.noreply.github.com>
2023-12-09 13:55:53 +0200

    Add UI test

--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -1,7 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add book page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-book');
+    //await page.goto('http://localhost:8080/add-book');
+    await page.goto('https://kuzmanovb-prepare-retake-exam1.onrender.com/add-book');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
